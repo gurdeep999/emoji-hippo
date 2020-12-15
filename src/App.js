e605bf18609e1cc4df9e4204f18748616d6100f8
Gurdeep Singh Saini <gurdeeprocksman2@gmail.com>
2020-12-15 22:05:46 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     if (emojiDataJSON) {
       return JSON.parse(emojiDataJSON)
     } else {
-      let response = await fetch('https://radiant-precept-290311-default-rtdb.firebaseio.com/api/emojis.json')
+      let response = await fetch('process.env.FIREBASE_URI')
       let data = await response.json()
       window.localStorage.setItem('emojiData', JSON.stringify(data))
       return data
